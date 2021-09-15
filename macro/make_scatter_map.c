fceac9b48123ad1fb6366abdd6646e6e2350cc37
Ka Ming Tsui <K.M.Tsui@liverpool.ac.uk>
2021-09-15 11:22:50 +0100

    Macro to produce scattering map for BnL fit

---
+++ macro/make_scatter_map.c
@@ -0,0 +1,61 @@
+// Use with WCSIM_TreeConvert output with -d options to produce a maping of indirect light background
+void make_scatter_map(){
+
+    TChain* fChain_digitized = new TChain("hitRate_pmtType0");
+    fChain_digitized->Add("/bundle/data/T2K/users/kmtsui/LI/production/out_diffuser4_350nm_nominal_*.root");
+
+    TFile* f = new TFile(fChain_digitized->GetFile()->GetName());
+    TTree* t = (TTree*)f->Get("pmt_type0");
+    double R, costh, cosths, omega, phim;
+    t->SetBranchAddress("R",&R);
+    t->SetBranchAddress("costh",&costh);
+    t->SetBranchAddress("cosths",&cosths);
+    t->SetBranchAddress("omega",&omega);
+    t->SetBranchAddress("phim",&phim);
+    const int nPMTs = t->GetEntries();
+    std::cout<<"nPMTs = "<<nPMTs<<std::endl;
+    std::vector<double> cosths_array, costh_array, R_array;
+
+    for (int i=0;i<nPMTs;i++){
+         t->GetEntry(i);
+         cosths_array.push_back(cosths);
+         costh_array.push_back(costh);
+         R_array.push_back(R);
+    }
+
+    double nPE, timetof, nPE_digi, timetof_digi;
+    int PMT_id, nRaySct, nReflec;
+    fChain_digitized->SetBranchAddress("nPE",&nPE);
+    fChain_digitized->SetBranchAddress("timetof",&timetof);
+    fChain_digitized->SetBranchAddress("PMT_id",&PMT_id);
+    fChain_digitized->SetBranchAddress("nRaySct",&nRaySct);
+    fChain_digitized->SetBranchAddress("nReflec",&nReflec);
+    fChain_digitized->SetBranchAddress("nPE_digi",&nPE_digi);
+    fChain_digitized->SetBranchAddress("timetof_digi",&timetof_digi);
+    TH1D* h_signal_region = new TH1D("","",nPMTs,0,nPMTs);
+    TH1D* h_control_region = new TH1D("","",nPMTs,0,nPMTs);
+    for (unsigned long int i=0;i<fChain_digitized->GetEntries();i++){
+        fChain_digitized->GetEntry(i);
+        if (nRaySct>0 || (nReflec>0 && timetof>0.1)) // scattered light is of course indirect light, but not all reflected light is
+        {
+            if (timetof_digi<15) h_signal_region->Fill(PMT_id+0.5,nPE_digi);
+            else if (timetof_digi<200) h_control_region->Fill(PMT_id+0.5,nPE_digi);
+        }
+    }
+
+    TFile* fout = new TFile("diffuser4_350nm_scattering_map.root","RECREATE");
+    for (int i=1;i<=h_signal_region->GetNbinsX();i++)
+    {
+        double x = h_control_region->GetBinContent(i);
+        double y = h_signal_region->GetBinContent(i);
+        if (x>0&&y>0)
+        {
+            double val = y/x;
+            double err = sqrt(1./x+1./y);
+            h_signal_region->SetBinContent(i,val);
+            h_signal_region->SetBinError(i,err);
+        }
+    }
+    h_signal_region->Write("scattering_map_15_200");
+    fout->Close();
+}
